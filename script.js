dbbe8b1dcadfb640d646dd0becafbfe51c397f64
Iuliia-D <iuliia.derevleva@gmail.com>
2022-09-13 14:43:58 +0200

    Experimenting with modal windows
    Nothing gets works :D

--- script.js
+++ script.js
@@ -14,18 +14,90 @@ document.querySelector('.header__menu').addEventListener('click', function (e) {
 // const accordion = document.querySelectorAll('.accordion');
 // const accordionContent = document.querySelectorAll('.panel');
 
-document.querySelectorAll('.accordion').forEach(a =>
-  a.addEventListener('click', function (e) {
-    this.classList.toggle('active');
-    let panel = this.nextElementSibling;
+// document.querySelectorAll('.accordion').forEach(a =>
+//   a.addEventListener('click', function (e) {
+//     this.classList.toggle('active');
+//     let panel = this.nextElementSibling;
 
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 'px';
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + 'px';
+//     }
+//   })
+// );
+
+///////////////////////////////////////
+// Modal window
+// const mod = document.querySelector('.mod');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelectorAll('.btn--close-modal');
+// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+// const openModal = function (e) {
+//   e.preventDefault();
+//   mod.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
+
+// const closeModal = function () {
+//   mod.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
+
+// btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// // for (let i = 0; i < btnsOpenModal.length; i++)
+// //   btnsOpenModal[i].addEventListener('click', openModal);
+
+// //btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
+
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !mod.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
+
+//const modSection = document.querySelector('.section_2');
+const mod = document.querySelectorAll('.mod');
+const overlay = document.querySelectorAll('.overlay');
+const btnCloseModal = document.querySelectorAll('.btn--close-modal');
+const btnsOpenModal = document.querySelector('.btn--show-modal');
+
+const acc = document.querySelectorAll('.accordion');
+
+(function Modal() {
+  const backdrop = document.querySelector('#modal-backdrop');
+  document.addEventListener('click', modalHandler);
+
+  function modalHandler(evt) {
+    const modalBtnOpen = evt.target.closest('.btn--show-modal');
+    if (modalBtnOpen) {
+      //open btn click
+      const modalSelector = modalBtnOpen.dataset.modal;
+      showModal(document.querySelector(modalSelector));
     }
-  })
-);
+    const modalBtnClose = evt.target.closest('.btn--close-modal');
+    if (modalBtnClose) {
+      //close btn click
+      evt.preventDefault();
+      hideModal(modalBtnClose.closest('.mod'));
+    }
+    if (evt.target.matches('#modal-backdrop')) {
+      //backdrop click
+      hideModal(document.querySelector('.mod.show'));
+    }
+  }
+  function showModal(modalElem) {
+    modalElem.classList.add('show');
+    backdrop.classList.remove('hiden');
+  }
+  function hideModal(modalElem) {
+    modalElem.classList.remove('.show');
+    backdrop.classList.add('hiden');
+  }
+})();
 
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
