b79e2851fdf1cac3306db369cf135badfe0205b4
Iuliia-D <iuliia.derevleva@gmail.com>
2022-09-14 13:38:47 +0200

    Add Page navigation footer

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 'use strict';
 
-// Page navigation
+// Page navigation header
 document.querySelector('.header__menu').addEventListener('click', function (e) {
   e.preventDefault();
   if (e.target.classList.contains('header__menu--btn')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+// Page navigation footer
+document.querySelector('.footer__menu').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('footer__menu--btn')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
@@ -43,12 +51,11 @@ menu.addEventListener('mouseout', handlerHoverMenu.bind(1));
 
 // Social fade animation
 const social = document.querySelector('.section1__Social');
-console.log(social);
 
 const handlerHoverSocial = function (e) {
   if (e.target.classList.contains('section1__Social--btn')) {
     const link = e.target;
-    console.log(link);
+
     const siblings = link
       .closest('.section1__Social')
       .querySelectorAll('.section1__Social--btn');
@@ -56,8 +63,39 @@ const handlerHoverSocial = function (e) {
     siblings.forEach(el => {
       if (el !== link) el.style.opacity = this;
     });
-    console.log(this);
   }
 };
 social.addEventListener('mouseover', handlerHoverSocial.bind(0.5));
 social.addEventListener('mouseout', handlerHoverSocial.bind(1));
+
+// // Sticy navigation
+// const section2 = document.querySelector('.section_2');
+// const initialCoords = section2.getBoundingClientRect();
+
+// window.addEventListener('scroll', function () {
+//   //console.log(this.window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) menu.classList.add('sticky');
+//   else menu.classList.remove('sticky');
+// });
+
+// Sticy navigation: Intersection Observer API
+// const main = document.querySelector('.main');
+// const menuHeight = menu.getBoundingClientRect().height;
+// console.log(menuHeight);
+
+// const stickyMenu = function (entries) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   if (!entry.isIntersecting) menu.classList.add('sticky');
+//   else menu.classList.remove('sticky');
+// };
+
+// const mainObserver = new IntersectionObserver(stickyMenu, {
+//   root: null,
+//   theshold: 0,
+//   rootMargin: `-${menuHeight}px`,
+// });
+
+// mainObserver.observe(main);
